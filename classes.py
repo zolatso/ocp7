3c014b094bd94753b32d8a38940b02aad2060d09
zolatso <tomsaunders1@gmail.com>
2025-03-21 09:30:33 +0100

    cleaning

--- classes.py
+++ classes.py
@@ -1,11 +1,9 @@
-
-
 class Action:
     def __init__(self, title, price, percentage):
         self.title = title
         self.price = price
         self.percentage = percentage
-        self.absolute_return = price * (percentage / 100)
+        self.absolute_return = round(price * (percentage / 100), 2)
 
 
 class Portfolio:
@@ -31,21 +29,7 @@ class Portfolio:
     
 
 def main():
-    portfolio = Portfolio()
-    action1 = Action('egg', 500, 10)
-    action2 = Action('cheese', 400, 20)
-
-    
-    portfolio.actions.append(action1)
-    portfolio.actions.append(action2)
-    portfolio.get_portfolio_cost()
-    portfolio.get_portfolio_return()
-
-    print("Name: Price / Percentage / Actual return")
-    for action in portfolio.actions:
-        print(f"{action.title}: {action.price, action.percentage, action.absolute_return}")
-    print(f"Total cost: {portfolio.cost}")
-    print(f"Total return: {portfolio.total_return}")
+    pass
 
 if __name__ == "__main__":
     main()
--- csv_reader.py
+++ csv_reader.py
@@ -27,21 +27,7 @@ def extract_data_class(file):
     return portfolio
 
 def main():
-    portfolio = Portfolio()
-    action1 = Action('egg', 500, 10)
-    action2 = Action('cheese', 400, 20)
-
-    
-    portfolio.actions.append(action1)
-    portfolio.actions.append(action2)
-    portfolio.get_portfolio_cost()
-    portfolio.get_portfolio_return()
-
-    print("Name: Price / Percentage / Actual return")
-    for action in portfolio.actions:
-        print(f"{action.title}: {action.price, action.percentage, action.absolute_return}")
-    print(f"Total cost: {portfolio.cost}")
-    print(f"Total return: {portfolio.total_return}")
+    pass
 
 if __name__ == "__main__":
     main()
